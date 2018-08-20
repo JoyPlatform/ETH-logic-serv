18348012d06f8389b1d2d364791eeceeee4d108e
Michal-JoyGaming <Michal-JoyGaming@gmail.com>
2018-08-20 08:48:13 +0200

    "Install new dep. Create basic functions"

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,16 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 
-export default ({ config, db }) => {
+export default ({ config }) => {
 	let api = Router();
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
+	});
+
+	api.get('/test', (req, res) => {
+		res.send('API - Test - Debug');
 	});
 
 	return api;
