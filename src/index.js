84aa8fd1ba921ce0cdd1f131135d52ceecaad0ed
Slawek-JoyGaming <Slawek-JoyGaming@gmail.com>
2018-11-05 08:00:17 +0100

    "Upgrade main switch, add new commands, better responses"

--- src/index.js
+++ src/index.js
@@ -42,53 +42,100 @@ wss.on('connection', (ws, req) => {
 
       case 'getBalance':
         try {
-          let address = messageJSON.address;
-          switch (messageJSON.unit) {
-            case 'Eth':
-              api.getBalanceEth(address)
-                .then(ethBalance => {
-                  let response = new Object({});
-                  response.command = messageJSON.command + '_RES';
-                  response.user = address;
-                  response.unit = messageJSON.unit;
-                  response.balance = ethBalance;
-                  ws.send(JSON.stringify(response));
-                });
-              break;
-            case 'Wei':
-              api.getBalanceWei(address)
-                .then(weiBalance => {
-                  let response = new Object({});
-                  response.command = messageJSON.command + '_RES';
-                  response.user = address;
-                  response.unit = messageJSON.unit;
-                  response.balance = weiBalance;
-                  ws.send(JSON.stringify(response));
-                });
-              break;
-            case 'JoyAsset':
-              api.getBalanceJoyCoin(address)
-                .then(joyBalance => {
-                  let response = new Object({});
-                  response.command = messageJSON.command + '_RES';
-                  response.user = address;
-                  response.unit = messageJSON.unit;
-                  response.balance = joyBalance;
-                  ws.send(JSON.stringify(response));
-                });
+          let address = messageJSON.user;
+          let location = messageJSON.location;
+          if (location === 'world') {
+            switch (messageJSON.unit) {
+              case 'Eth':
+                api.getBalanceEth(address)
+                  .then(ethBalance => {
+                    let response = new Object({});
+                    response.command = messageJSON.command + '_RES';
+                    response.user = address;
+                    response.location = 'world';
+                    response.unit = messageJSON.unit;
+                    response.balance = ethBalance;
+                    response.status = 0;
+                    ws.send(JSON.stringify(response));
+                  });
+                break;
+
+              case 'Wei':
+                api.getBalanceWei(address)
+                  .then(weiBalance => {
+                    let response = new Object({});
+                    response.command = messageJSON.command + '_RES';
+                    response.user = address;
+                    response.location = 'world';
+                    response.unit = messageJSON.unit;
+                    response.balance = weiBalance;
+                    response.status = 0;
+                    ws.send(JSON.stringify(response));
+                  });
+                break;
+
+              case 'JoyAsset':
+                api.getBalanceJoyCoin(address)
+                  .then(joyBalance => {
+                    let response = new Object({});
+                    response.command = messageJSON.command + '_RES';
+                    response.user = address;
+                    response.location = 'world';
+                    response.unit = messageJSON.unit;
+                    response.balance = joyBalance;
+                    response.status = 0;
+                    ws.send(JSON.stringify(response));
+                  });
+                break;
+            }
+          } else if (location === 'platform') {
+            if (messageJSON.unit != 'JoyAsset') {
+              let response = new Object({});
+              response.status = 1;
+              response.status_err = `Only JoyAsset is supported as platform balance`;
+              ws.send(JSON.stringify(response));
               break;
             }
+            api.getPlayerDeposit(address)
+              .then(playerFunds => {
+                let response = new Object({});
+                response.command = messageJSON.command + '_RES';
+                response.user = address;
+                response.location = 'platform';
+                response.unit = messageJSON.unit;
+                response.balance = playerFunds;
+                response.status = 0;
+                ws.send(JSON.stringify(response));
+              });
+          } else if (location === 'gameSession' ) {
+            if (messageJSON.unit != 'JoyAsset') {
+              let response = new Object({});
+              response.status = 1;
+              response.status_err = `Only JoyAsset is used in gameSession`;
+              ws.send(JSON.stringify(response));
+              break;
+            }
+            api.getPlayerDepositLocked(address)
+              .then(playerLockedFunds => {
+                let response = new Object({});
+                response.command = messageJSON.command + '_RES';
+                response.user = address;
+                response.location = 'gameSession';
+                response.unit = messageJSON.unit;
+                response.balance = playerLockedFunds;
+                response.status = 0;
+                ws.send(JSON.stringify(response));
+              });
+          } else {
+            ws.send(`Missing Json parameter: "location" : [ "world", "platform", "gameSession" ]`);
+          }
         } catch (e) {
           console.log(`> error - parse JSON: ${e}`);
           ws.send(`"JSON error ${ e }"`);
         }
         break;
 
-
-      case 'getBalanceJoy':
-        let address = messageJSON.address;
-        break;
-
+      //------------------------------------------ debug informations
       case 'accountsInfo':
         api.accountsInfo(ws);
         break;
@@ -97,10 +144,10 @@ wss.on('connection', (ws, req) => {
         api.debugContractsInfo();
         break;
 
-      // only debug information to console
       case 'getToken':
         api.testTokenContract();
         break;
+      //------------------------------------------
       default:
         console.log(`> pong: ${message}`);
         ws.send(`pong: ${message}`);
